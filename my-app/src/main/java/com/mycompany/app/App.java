24349c9f469ee17a4b0a3b6c4b86966f3ccceceb
rksundaram76 <81460613+rksundaram76@users.noreply.github.com>
2022-10-26 10:28:55 +0000

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World! Jenkins Amazing!!!!" );
         System.out.println( "Some Random Edits........" );
+        System.out.println( "One More.... Some Random Edits........" );
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
         /**
